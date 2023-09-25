ab33ff14f1dcaa6035f5711a2a5b44d3d2d1ec1d
Lyove <lyove@outlook.com>
2023-09-25 16:02:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import DomHandler from "./domHandler";
 import EventBus from "./eventBus";
 import ObjectUtils from "./objectUtils";
+import HtmlUtils from "./htmlUtils";
 import Http from "./http";
 import { getUrlParam, getUrlParams } from "./url";
 
-export { DomHandler, EventBus, ObjectUtils, Http, getUrlParam, getUrlParams };
+export { DomHandler, EventBus, ObjectUtils, HtmlUtils, Http, getUrlParam, getUrlParams };
